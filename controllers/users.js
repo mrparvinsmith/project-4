e6448a8142ec0bb73f9dddfd46b874b855029220
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-05-23 11:41:15 -0700

    added model data and user-controller functions

--- controllers/users.js
+++ controllers/users.js
@@ -2,23 +2,56 @@ var User = require('../models/user');
 var controller = {};
 
 controller.index = function(req, res){
-
+  User.find({}, function(err, users){
+    if(err) throw err;
+    res.json(users);
+  });
 };
 
 controller.create = function(req, res){
-
+  var user = new User();
+  user.username = req.body.username;
+  user.password = user.encrypt(req.body.password);
+  user.save(function(err){
+    if (err) throw err;
+    res.json(user);
+  });
 };
 
 controller.show = function(req, res){
-
+  User.findById(req.params.id, function(err, user){
+    if(err) throw err;
+    res.json(user);
+  });
 };
 
 controller.update = function(req, res){
-
+  User.findById(req.params.id, function(err, user){
+    if(err) throw err;
+    req.body.routes.forEach(function(route){
+      user.favorites.routes.push(route);
+    });
+    req.body.stations.forEach(function(station){
+      user.favorites.stations.push(station);
+    });
+    req.body.bases.forEach(function(base){
+      user.favorites.bases.push(base);
+    });
+    // user.favorites.routes.push(req.body.routes);
+    // user.favorites.stations.push(req.body.stations);
+    // user.favorites.bases.push(req.body.bases);
+    user.save(function(err){
+      if(err) throw err;
+      res.json(user);
+    });
+  });
 };
 
 controller.destroy = function(req, res){
-
+  User.findByIdAndRemove(req.params.id, function(err){
+    if(err) throw err;
+    res.json({message: 'User deleted'});
+  });
 };
 
 module.exports = controller;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
+var userSchema = new Schema({
+  username: String,
+  password: String,
+  favorites: {
+    routes: [{type: String}],
+    stations: [{type: String}],
 
-var User = {};
+    //the basis of 'search-nears' like home or work
+    bases:[{type: String}]
+  }
+});
+
+userSchema.methods.validPassword = function(pwd){
+  return bcrypt.compareSync(pwd, this.password);
+};
+
+userSchema.methods.encrypt = function(pwd){
+  return bcrypt.hashSync(pwd, 8);
+};
+
+var User = mongoose.model('User', userSchema);
 
 module.exports = User;
