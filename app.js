f5e4d6f5965cb66777c5927d3e9f1f957391df33
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-05-23 12:27:08 -0700

    added welcome page

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var db = require('./config/db');
 
 var app = express();
 
+app.use( express.static(path.join(__dirname + '/public')));
+
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/', require('./routes/index'));
 app.use('/api/users', require('./routes/users'));
 
 var port = process.env.PORT || 3000;
