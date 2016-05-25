237f920904e43b7eb59197ae9c083e9d62730d36
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-05-24 17:12:47 -0700

    can get user location; but won't display immediately

--- public/js/app.js
+++ public/js/app.js
@@ -8,11 +8,9 @@ function configuration($stateProvider, $urlRouterProvider){
     .state('login', {
       url:'/login',
       templateUrl: 'partials/login.html',
-      // controller: 'LoginController as ctrl'
     })
     .state('signup', {
       url: '/signup',
       templateUrl: 'partials/signup.html',
-      // controller: 'SignupController as ctrl'
     });
 }
--- public/js/mapController.js
+++ public/js/mapController.js
@@ -5,6 +5,10 @@ MapController.$inject = ['$http'];
 
 function MapController($http){
   var self = this;
+  self.location = [];
+  self.garages = [];
+  self.metroStations = [];
+  self.changeLocation = changeLocation;
 
   if(JSON.parse(localStorage.getItem('token'))){
     self.loggedIn = true;
@@ -21,8 +25,40 @@ function MapController($http){
     self.loggedIn = false;
   }
 
+  function getLocation(){
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var location = position.coords.latitude + ', '  + position.coords.longitude;
+        self.location.push(location);
+        console.log(self.location);
+      });
+    } else {
+      self.positionUnknown = true;
+    }
+  }
+  getLocation();
+
+  // turn into function?
+  if(self.search){
+    if(self.search.garages){
+      var garageAcceptHeader = {headers: {'Accept': 'application/json'}};
+      $http.get('https://parking.api.smgov.net/lots')
+        .then(function(response){
+          console.log(response);
+        });
+    } else {
+      self.garages = [];
+    }
+  }
   // setting up interval
   // self.interval = interval
   // function interval(){}
   // self.interval()
+
+  function changeLocation(){
+    self.location.unshift(self.newLocation);
+    self.newLocation = '';
+    console.log(self.location);
+  }
+
 }
