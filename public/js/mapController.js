32b96dc32fc63b19a0a92204b5bbd9cf3784d514
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-05-25 10:17:07 -0700

    glitchy work-around for showing user location; garage api working

--- public/js/mapController.js
+++ public/js/mapController.js
@@ -5,10 +5,13 @@ MapController.$inject = ['$http'];
 
 function MapController($http){
   var self = this;
-  self.location = [];
+  self.location = '';
   self.garages = [];
   self.metroStations = [];
   self.changeLocation = changeLocation;
+  self.getLocation = getLocation;
+  self.searchGarages = searchGarages;
+  self.searchMetro = searchMetro;
 
   if(JSON.parse(localStorage.getItem('token'))){
     self.loggedIn = true;
@@ -29,8 +32,9 @@ function MapController($http){
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var location = position.coords.latitude + ', '  + position.coords.longitude;
-        self.location.push(location);
+        self.location = location;
         console.log(self.location);
+        return self.location;
       });
     } else {
       self.positionUnknown = true;
@@ -38,25 +42,29 @@ function MapController($http){
   }
   getLocation();
 
-  // turn into function?
-  if(self.search){
-    if(self.search.garages){
-      var garageAcceptHeader = {headers: {'Accept': 'application/json'}};
-      $http.get('https://parking.api.smgov.net/lots')
-        .then(function(response){
-          console.log(response);
+  function searchGarages(){
+    self.garages = [];
+    var garageAcceptHeader = {headers: {'Accept': 'application/json'}};
+    $http.get('https://parking.api.smgov.net/lots')
+      .then(function(response){
+        console.log(response);
+        response.data.forEach(function(garage){
+          self.garages.push(garage);
         });
-    } else {
-      self.garages = [];
-    }
+      });
   }
+
+  function searchMetro(){
+    self.metro = [];
+  }
+
   // setting up interval
   // self.interval = interval
   // function interval(){}
   // self.interval()
 
   function changeLocation(){
-    self.location.unshift(self.newLocation);
+    self.location = self.newLocation;
     self.newLocation = '';
     console.log(self.location);
   }
