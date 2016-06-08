2aeb353b791c1a57d1ed709124dee4ac65435ca5
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-06-08 03:02:11 -0700

    cleaned up some dead code

--- controllers/token.js
+++ controllers/token.js
@@ -7,10 +7,6 @@ controller.findUser = function(req, res, next){
   User.findOne({username: req.body.username})
     .then(function(user){
       if(!user){
-        // next({
-        //   status: 401,
-        //   message: 'Authentication failed: credentials incorrect'
-        // });
         res.json({error: 'Login credentials incorrect'});
       }
       req.user = user;
@@ -24,10 +20,6 @@ controller.findUser = function(req, res, next){
 controller.validateUser = function(req, res, next){
   req.user.verifyPassword(req.body.password, function(err, valid){
     if(!valid){
-      // next({
-      //   status: 401,
-      //   message: 'Authentication failed: credentials incorrect'
-      // });
       res.json({error: 'Login credentials incorrect'});
     } else {
       next();
